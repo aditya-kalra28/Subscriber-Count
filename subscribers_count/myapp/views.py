f7f333f90f9f275d086f3010726874d054404425
Aditya <aditya.kalra28@gmail.com>
2022-03-14 14:45:28 +0530

    initial commit

---
+++ subscribers_count/myapp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+import urllib.request
+import json
+
+# Create your views here.
+
+def index(request):
+    if request.method=='POST':
+        channel=request.POST['channel']
+        try:
+            req = req = urllib.request.urlopen('https://www.googleapis.com/youtube/v3/channels?forUsername='+channel+'&key=AIzaSyBAiqtwaDhw4zWWrvp85MNPUch1JwHzZac&part=statistics')
+            details=req.read()
+            details_dict=json.loads(details)
+            print(details_dict)
+            data={
+                'channel':channel,
+                'subscriberCount':details_dict['items'][0]['statistics']['subscriberCount'],
+                'viewCount':details_dict['items'][0]['statistics']['viewCount'],
+                'videoCount':details_dict['items'][0]['statistics']['videoCount'],
+                'error':False
+            }
+        except urllib.error.HTTPError as exception:
+            data={
+                'error':True
+            }
+    else:
+        city=''
+        data={'city':city, 'error':False}
+    return render(request,'index.html',data)
+    # OR we can pass (request,'index.hmtl',{'city':city,'data':data})
+
+
